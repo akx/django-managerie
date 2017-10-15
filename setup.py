8f9299e76fd6a6d3696131464b573d9fef1298d0
Aarni Koskela <akx@iki.fi>
2017-10-15 21:11:44 +0300

    Add bits for distribution

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools import setup, find_packages
 setup(
     name='django-managerie',
     version='0.0',
+    author='Aarni Koskela',
+    author_email='akx@iki.fi',
     packages=find_packages(include=('django_managerie', 'django_managerie*')),
+    include_package_data=True,
     install_requires=['Django', 'wrapt'],
 )
