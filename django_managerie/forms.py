6621e4fe6fdc2cbb77501854377d42d4ba386715
Aarni Koskela <akx@iki.fi>
2017-10-15 19:35:00 +0300

    Initial code

---
+++ django_managerie/forms.py
@@ -0,0 +1,42 @@
+# -- encoding: UTF-8 --
+from argparse import _HelpAction, _StoreAction, _StoreTrueAction
+
+from django import forms
+
+FIELD_CLASS_MAP = {'int': forms.IntegerField, }
+
+
+class ArgumentParserForm(forms.Form):
+    IGNORED_DESTS = {
+        'pythonpath',
+        'settings',
+        #'no_color',  # TODO: Remove me
+        'traceback',
+    }
+    def __init__(self, *, parser, **kwargs):
+        """
+        :type parser: argparse.ArgumentParser
+        """
+        self.parser = parser
+        super(ArgumentParserForm, self).__init__(**kwargs)
+        for action in self.parser._actions:
+            print(action)
+            if isinstance(action, _HelpAction):
+                continue
+            if action.dest in self.IGNORED_DESTS:
+                continue
+            field_cls = None
+            field_kwargs = dict(
+                initial=action.default,
+                label=action.dest.replace('_', ' ').title(),
+                help_text=action.help,
+                required=action.required,
+            )
+            if isinstance(action, _StoreTrueAction):
+                field_cls = forms.BooleanField
+            elif isinstance(action, _StoreAction):
+                field_cls = FIELD_CLASS_MAP.get(action.type, forms.Field)
+            if field_cls:
+                self.fields[action.dest] = field_cls(**field_kwargs)
+
+            # TODO: Probably support for more fields :)
---
+++ django_managerie/views.py
@@ -0,0 +1,82 @@
+import io
+import traceback
+from contextlib import redirect_stdout, redirect_stderr
+
+from django.apps import apps
+from django.core.management import load_command_class, CommandParser
+from django.views.generic import TemplateView, FormView
+
+from django_managerie.forms import ArgumentParserForm
+
+
+class MenagerieBaseMixin:
+    managerie = None
+
+    def get_command_instance(self):
+        return load_command_class(self.kwargs['app_label'], self.kwargs['command'])
+
+    def get_app(self):
+        if hasattr(self, '_app'):
+            return self._app
+        self._app = apps.get_app_config(self.kwargs['app_label'])
+        return self._app
+
+
+class ManagerieListView(MenagerieBaseMixin, TemplateView):
+    template_name = 'django_managerie/admin/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ManagerieListView, self).get_context_data(**kwargs)
+        context['app'] = app = self.get_app()
+        context['commands'] = self.managerie.get_commands()[app.label].values()
+        return context
+
+
+class ManagerieCommandView(MenagerieBaseMixin, FormView):
+    template_name = 'django_managerie/admin/command.html'
+
+    def get_form(self, form_class=None):
+        cmd = self.get_command_instance()
+        parser = cmd.create_parser('django', self.kwargs['command'])
+        return ArgumentParserForm(parser=parser, **self.get_form_kwargs())
+
+    def get_context_data(self, **kwargs):
+        context = super(ManagerieCommandView, self).get_context_data(**kwargs)
+        context['app'] = app = self.get_app()
+        cmd_name = self.kwargs['command']
+        context['command'] = self.managerie.get_commands()[app.label][cmd_name]
+        context['command_help'] = self.get_command_instance().help
+        return context
+
+    def form_valid(self, form):
+        # This mimics BaseCommand.run_from_argv():
+        options = dict(form.cleaned_data)
+        args = options.pop('args', ())  # "Move positional args out of options to mimic legacy optparse"
+        stdout = io.StringIO()
+        stderr = io.StringIO()
+        error = None
+        error_tb = None
+        with redirect_stdout(stdout), redirect_stderr(stderr):
+            options.update({
+                'traceback': True,
+                'no_color': True,
+                'stdout': stdout,
+                'stderr': stderr,
+            })
+            cmd = self.get_command_instance()
+            try:
+                cmd.execute(*args, **options)
+            except SystemExit as se:  # We don't want any stray sys.exit()s to quit the app server
+                stderr.write('<exit: %s>' % se)
+            except Exception as exc:
+                error = exc
+                error_tb = traceback.format_exc()
+        context = self.get_context_data(
+            executed=True,
+            form=form,
+            error=error,
+            error_tb=error_tb,
+            stdout=stdout.getvalue(),
+            stderr=stderr.getvalue(),
+        )
+        return self.render_to_response(context=context, status=(400 if error else 200))
