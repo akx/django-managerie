66d623f1a7d5502db5b90d9c5e0b63d6add34e38
Aarni Koskela <akx@iki.fi>
2017-10-15 19:57:55 +0300

    Cross some Ts, dot some Is

---
+++ django_managerie/compat.py
@@ -0,0 +1,30 @@
+# -- encoding: UTF-8 --
+
+import sys
+
+# From `contextlib` in Python 3.5.
+
+
+class _RedirectStream:
+    _stream = None
+
+    def __init__(self, new_target):
+        self._new_target = new_target
+        # We use a list of old targets to make this CM re-entrant
+        self._old_targets = []
+
+    def __enter__(self):
+        self._old_targets.append(getattr(sys, self._stream))
+        setattr(sys, self._stream, self._new_target)
+        return self._new_target
+
+    def __exit__(self, exctype, excinst, exctb):
+        setattr(sys, self._stream, self._old_targets.pop())
+
+
+class redirect_stdout(_RedirectStream):
+    _stream = "stdout"
+
+
+class redirect_stderr(_RedirectStream):
+    _stream = "stderr"
