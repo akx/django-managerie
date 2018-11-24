b02b5d85ac56d861bb670ec87f2c7581c066943c
Aarni Koskela <akx@iki.fi>
2018-11-24 14:20:03 +0200

    Blacklist some management commands

--- managerie_tests/test_managerie.py
+++ managerie_tests/test_managerie.py
@@ -4,8 +4,8 @@ import pytest
 @pytest.mark.django_db
 def test_managerie(admin_client):
     assert 'Commands' in admin_client.get('/admin/').content.decode()
-    chpw_content = admin_client.get('/admin/managerie/auth/changepassword/').content.decode()
-    assert 'password for django.contrib.auth.' in chpw_content
-    assert 'Verbosity' in chpw_content
-    assert 'Username' in chpw_content
-    assert 'Database' in chpw_content
+    csup_content = admin_client.get('/admin/managerie/auth/createsuperuser/').content.decode()
+    assert 'Used to create a superuser.' in csup_content
+    assert 'Verbosity' in csup_content
+    assert 'Username' in csup_content
+    assert 'Database' in csup_content
